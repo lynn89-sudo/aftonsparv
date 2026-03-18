99c0df1dda860fbcddc81aef7f240516c84261a2
Lynn <luke87joseph@gmail.com>
2026-03-18 14:01:28 +0000

    Actually fix the issue

---
+++ docs/_app/immutable/chunks/CpOznZRq.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_f2u9x5?.base??"/aftonsparv",a=globalThis.__sveltekit_f2u9x5?.assets??s??"";export{a,s as b};
--- docs/_app/immutable/entry/start.DeryJgIW.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/B8q38LMk.js";export{o as load_css,r as start};
